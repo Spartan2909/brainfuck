cc41932357d2121c6ce14800ed436f4d105cff78
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2022-11-03 15:29:55 +0000

    Improve error handling

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,32 @@ fn find_matching_bracket(start_index: usize, program: &str) -> usize {
     return 0 as usize;
 }
 
+fn check_brackets_match(program: &str) -> (bool, usize) {
+    let mut open_brackets = 0;
+    let mut last_open_bracket = 0;
+    let mut last_close_bracket = 0;
+    for (i, item) in program.chars().enumerate() {
+        if item == '[' {
+            last_open_bracket = i;
+            open_brackets += 1;
+        } else if item == ']' {
+            last_close_bracket = i;
+            open_brackets -= 1;
+        }
+    }
+
+    let brackets_match = open_brackets == 0;
+    
+    let mut mismatched = 0;
+    if open_brackets > 0 {
+        mismatched = last_open_bracket;
+    } else if open_brackets < 0 {
+        mismatched = last_close_bracket;
+    }
+
+    return (brackets_match, mismatched);
+}
+
 fn find_location(location: usize, program: &str) -> (usize, usize) {
     let line_num = program[0..location].matches("\n").count() + 1;
 
@@ -52,6 +78,11 @@ fn overflow_error(location: usize, program: &str) -> String {
     return format!("{}:{} - Overflow Error", readable_location.0, readable_location.1);
 }
 
+fn syntax_error(location: usize, problem: &str, program: &str) -> String {
+    let readable_location = find_location(location, program);
+    return format!("{}:{} - Syntax Error - {}", readable_location.0, readable_location.1, problem);
+}
+
 fn raise_error(error: String) {
     println!("{}", &error);
     process::exit(1);
@@ -63,6 +94,11 @@ fn main() {
 
     let program = fs::read_to_string(file_path)
         .expect("File failed to read");
+
+    let check_match = check_brackets_match(&program);
+    if !check_match.0 {
+        raise_error(syntax_error(check_match.1, "Unmatched bracket", &program))
+    }
 
     let mut ptr = 0 as usize;
     let mut arr: [u8; 65535] = [0; 65535];

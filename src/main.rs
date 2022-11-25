d245c05a926e2357b9e6f12aa7fb0b6c0ba73dfe
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2022-11-25 21:59:25 +0000

    Add docs link

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,7 @@ impl ExecutionError {
 
 impl fmt::Display for ExecutionError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f,"{}",self.details)
+        write!(f, "{}", self.details)
     }
 }
 
